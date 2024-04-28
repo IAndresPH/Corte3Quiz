cbf68003217eb19f5e9b3ec3b2c2ad48c8bc1702
IAndresPH <amolinaph@gmail.com>
2024-04-28 18:52:34 -0500

    Finalicación quiz

--- quiz1-corte3/angular/src/app/app-routing.module.ts
+++ quiz1-corte3/angular/src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { MyInputComponent } from './my-input/my-input.component';
 import { MyButtonComponent } from './my-button/my-button.component';
 import { MenuComponent } from './menu/menu.component';
-import { ButtonsComponent } from './components/buttons/buttons.component';
+import { MyLabelComponent } from './my-label/my-label.component';
 
 const routes: Routes = [
+  {path:'label', component: MyLabelComponent},
   {path:'input', component: MyInputComponent},
-  {path:'buttons', component: ButtonsComponent},
   {path:'button', component: MyButtonComponent},
   {path: 'home', component: MenuComponent},
   {path: '**', redirectTo: 'home', pathMatch: 'full'},
--- quiz1-corte3/angular/src/app/app.module.ts
+++ quiz1-corte3/angular/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, input } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -11,9 +11,11 @@ import { MyButtonComponent } from './my-button/my-button.component';
 import { MenuComponent } from './menu/menu.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
 import { InputsComponent } from './components/inputs/inputs.component';
+import { LabelsComponent } from './components/labels/labels.component';
+import { MyLabelComponent } from './my-label/my-label.component';
 
 @NgModule({
-  declarations: [AppComponent, MyInputComponent, MyButtonComponent, MenuComponent, ButtonsComponent, InputsComponent],
+  declarations: [AppComponent, MyInputComponent, MyButtonComponent, MenuComponent, ButtonsComponent, InputsComponent, LabelsComponent, MyLabelComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
