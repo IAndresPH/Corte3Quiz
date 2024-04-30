fc567e9d6238d3b32c444eb686df07e4b849ead1
Andrés Molina <128313106+IAndresPH@users.noreply.github.com>
2024-04-29 23:17:25 -0500

    Delete quiz2-corte3 directory

--- quiz2-corte3/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'quiz2-corte3',
-  webDir: 'dist'
-};
-
-export default config;
